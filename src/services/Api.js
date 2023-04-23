3cda091a2faa6c776610a9d03af8ad537854b5ed
Luis David Montes Mosquera <davidmos.2906@gmail.com>
2023-04-22 20:09:18 -0500

    Agregando la url de api AWS

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_URL = "http://52.23.253.109:5000/api/";
+//const API_URL = "http://localhost:5000/api/";
+const API_URL = "http://3.219.66.127:5000/api/";
 
 const instance = axios.create({
     withCredentials: false,
